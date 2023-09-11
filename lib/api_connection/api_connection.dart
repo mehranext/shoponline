5ae59320f5c99123225774693fed12a7c970428f
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-09-11 11:11:15 +0330

    lets connect the api and files with each other.

---
+++ lib/api_connection/api_connection.dart
@@ -0,0 +1,8 @@
+class API{
+
+  static const hostConnect = "http://192.168.2.74/api_shop";
+  //addressing User folder in api file
+  static const hostConnectUser = "$hostConnect/user";
+//signUp user in api file and addressing again
+ static const signUp = "$hostConnect/user/signup.php";
+}
