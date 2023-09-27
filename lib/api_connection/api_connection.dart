3f8b1918b3064fbcae94aa86836b76e7fd6dd656
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-09-27 10:24:14 +0330

    api connections and addressing should be set with eachother in VSC(php).

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,9 +1,9 @@
-class API{
-
-  static const hostConnect = "http://192.168.163.74/api_shop";
+class API {
+  static const hostConnect = "http://192.168.12.74/api_shop_app";
   //addressing User folder in api file
   static const hostConnectUser = "$hostConnect/user";
 //signUp user in api and validate file and addressing again
   static const validateEmail = "$hostConnect/user/validate_email.php";
   static const signUp = "$hostConnect/user/signup.php";
+  static const login = "$hostConnect/user/login.php";
 }
