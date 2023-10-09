93f1f3a3f5d6afde637210c90e451123f3e7347a
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-10-09 14:48:54 +0330

    this page is crated just for that users which is login successful and after closing and opening again just automatically get back.

---
+++ lib/users/userPerferences/current_user.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:shoponline/users/model/user.dart';
+import 'package:shoponline/users/userPerferences/user_preferences.dart';
+
+class CurrentUser extends GetxController
+{
+
+  final Rx<User> _currentUser = User(0, '', '', '').obs;
+
+  User get user => _currentUser.value;
+  getUserInfo() async
+  {
+    User? getUserInfoFromLocalStorage =await RememberUserPrefs.readUserInfo();
+   _currentUser.value = getUserInfoFromLocalStorage!;
+  }
+
+}
