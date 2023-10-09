b579f070f84b6a1dbf51ffd61e04b5382265326b
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-10-09 14:38:05 +0330

    some change and add font awesome flutter to project.

--- lib/users/userPerferences/user_preferences.dart
+++ lib/users/userPerferences/user_preferences.dart
@@ -5,10 +5,25 @@ import 'package:shoponline/users/model/user.dart';
 
 class RememberUserPrefs {
   //save-remember user info
-  static Future<void> saveRememberUser (User userInfo) async
+  static Future<void> saveUserInfo (User userInfo) async
   {
   SharedPreferences preferences = await SharedPreferences.getInstance();
   String userJsonData = jsonEncode(userInfo.toJson());
   await preferences.setString("currentUser", userJsonData);
+  //"currentuser" is a key name that we use on current user and parts of save info.
   }
+
+  //get-read user info:
+static Future<User?> readUserInfo() async
+{
+    User? currentUserInfo ;
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+   String? userInfo = preferences.getString("currentUser");
+   if (userInfo !=null)
+     {
+    Map<String, dynamic> userDataMap = jsonDecode(userInfo);
+    currentUserInfo = User.fromJson(userDataMap);
+     }
+   return currentUserInfo;
+}
 }
