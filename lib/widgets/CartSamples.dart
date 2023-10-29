7f9fea52e53fc1d582e8392b5787251fbfdc68a0
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-10-29 14:48:15 +0330

    Design and explain an example with loop and our custom images .

---
+++ lib/widgets/CartSamples.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shoponline/consts/colors.dart';
+
+class CartItemSamples extends StatelessWidget {
+  const CartItemSamples({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (int i = 1; i<4; i++)
+        Container(
+        height: 110,
+        margin: EdgeInsets.symmetric(horizontal: 15 ,vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10
+          ),
+        ),
+        child: Row(children: [
+          Radio(value: "",
+            groupValue: "",
+            activeColor: darkblueColor,
+            onChanged: (index){},
+          ),
+          Container(
+            height: 70,
+            width: 70,
+            margin: EdgeInsets.only(right: 15),
+            //$i for changing images in our loop on the top,
+            child: Image.asset("images/$i.png"),
+          ),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 10),
+         child: Column(
+           crossAxisAlignment:CrossAxisAlignment.start ,
+         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         children: [
+           Text("Product Title",
+           style: TextStyle(
+             fontWeight: FontWeight.normal,
+             color: darkblueColor,
+             fontSize: 18,
+           ),
+           ),
+           Text("\$55",
+             style: TextStyle(
+               fontSize: 16,
+               fontWeight: FontWeight.normal,
+               color: darkblueColor,
+             ),
+           ),
+         ],
+         ),
+          ),
+          Spacer(),
+          Padding(padding: EdgeInsets.symmetric(vertical: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.delete,
+              color: Colors.red,
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 10,
+                        )
+                      ]
+                    ),
+                     child: Icon(CupertinoIcons.minus,
+                     size: 18,
+                     ),
+                  ),
+                  Container(margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("01",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 16,
+                      color: darkblueColor,
+                    ),),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            blurRadius: 10,
+                          )
+                        ]
+                    ),
+                    child: Icon(CupertinoIcons.plus,
+                      size: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ),
+        ],
+        ),
+      )],
+    );
+  }
+}
