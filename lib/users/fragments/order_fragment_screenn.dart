3a3a591c30d453e43c8e0578e81495667073df33
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-10-29 15:01:18 +0330

    Order and products UI parts. such as item , description , price and etc..

--- lib/users/fragments/order_fragment_screenn.dart
+++ lib/users/fragments/order_fragment_screenn.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shoponline/consts/colors.dart';
+import 'package:shoponline/widgets/CartBottomNavBar.dart';
+
+import '../../widgets/CartAppBar.dart';
+import '../../widgets/CartSamples.dart';
 
 class OrderFragmentScreen extends StatelessWidget
-{
 
+{
+  const OrderFragmentScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Order Fragment Screen"
-        ),
+      body: ListView(
+        children: [
+          CartAppbar(),
+
+          Container(
+          //temporary height
+            height: 700,
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(35),
+                topLeft: Radius.circular(35),
+              ),
+            ),
+            child: Column(children: [
+              CartItemSamples(),
+              Container(
+                // decoration: BoxDecoration(
+                //   borderRadius: BorderRadius.circular(10),
+                // ),
+                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 15),
+                padding: EdgeInsets.all(10),
+                child: Row(children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: darkblueColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Icon(Icons.add,color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                      padding:EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("Add Coupon Code",
+                    style: TextStyle(
+                    color: darkblueColor,
+                    fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                    ),
+                  ),
+                ],),
+              )
+            ],),
+          ),
+        ],
       ),
+      bottomNavigationBar:CartBottomNavBar() ,
     );
   }
 }
---
+++ lib/widgets/itemWidget.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import '../consts/colors.dart';
+
+class ItemsWidget extends StatelessWidget {
+  const ItemsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      //it disable the scroll functionality of gridview
+      //then it scroll in listview of home page
+      physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+      childAspectRatio: 0.68,
+      shrinkWrap: true,
+      children: [
+        for (int i= 1; i<8; i++)
+        Container(
+          padding: EdgeInsets.only(left: 15,right: 15,top: 10),
+          margin: EdgeInsets.symmetric(vertical: 8,horizontal: 10),
+          decoration: BoxDecoration(
+          color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              Row(
+                //the mainAxisAlignment space between is for space between like icon and discount
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("-50%",
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ),
+                  Icon(Icons.favorite_border,color: Colors.red,)
+                ],
+              ),
+              InkWell(onTap: (){},
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  //$i -> is related to for cycle that we create and
+                  //it bring our images to the cycle:)
+                  child: Image.asset("images/$i.png"),
+                  height: 120,
+                  width: 120,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(bottom: 8),
+                alignment: Alignment.centerLeft,
+                child: Text("Products Title",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: darkblueColor,
+                ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text("Write Description of product",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: darkblueColor,
+                ),
+                ),
+              ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 9),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("\$55",
+                  style: TextStyle(fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                    color: darkblueColor,
+                  ),
+                  ),
+                  Icon(Icons.shopping_cart_checkout,
+                    color: darkblueColor,
+                  ),
+                ],
+              ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
