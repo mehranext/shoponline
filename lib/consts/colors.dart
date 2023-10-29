162d864849cd21ece99d1c5bbb36cab6a4405405
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-10-29 14:44:57 +0330

    Our Colors (save to use easy).

---
+++ lib/consts/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xff1F212C);
+const whiteColor = Color(0xffFFFFFF);
+const sliderColor = Color(0xff7E70FF);
+const buttonColor = Color(0xff60E90D);
+const bgDarkColor = Color(0xff070B11);
+const darkblueColor=  Color(0xFF4C53A5);
