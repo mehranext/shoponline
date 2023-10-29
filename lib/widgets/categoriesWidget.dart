e735ec5ed2be571eef03882dd6ba48b36577ff7f
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-10-29 14:50:32 +0330

    Design and explain an example with loop and our custom images in OUR CATEGORIES .

---
+++ lib/widgets/categoriesWidget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CategoriesWidget extends StatelessWidget {
+  const CategoriesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (int i = 1; i<10;i++)
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          padding: EdgeInsets.symmetric(horizontal: 5,vertical: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Image.asset(
+                //using i variable to change pictures in loop :
+                "images/$i.png",
+                width: 40,
+                height: 40,
+              ),
+              Text("Bag",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+                color: Color(0XFF4C53A5),
+              ),)
+            ],
+          ),
+        )
+      ],),
+    );
+  }
+}
