994e5627f459e248bdb6467601ba832ec606d541
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-12-24 12:33:50 +0330

    wrap container to sizedbox for design and give sized to appbaar

--- lib/widgets/homeAppBar.dart
+++ lib/widgets/homeAppBar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:shoponline/widgets/Sidebar.dart';
+import 'package:badges/badges.dart';
 import '../users/fragments/order_fragment_screenn.dart';
 
 class HomeAppBar extends StatelessWidget {
@@ -8,45 +9,17 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(25),
-      child: Row(
-        children: [
-          Icon(Icons.sort,
-            size: 30,
-            color: Color(0xFF4C53A5),
-          ),
-          Padding(padding: EdgeInsets.only(left: 20,
-          ),
-            child: Text(
-              "DR Shop",
+    return  SizedBox(height:75,
+      child: Container(
+        child: AppBar(
+          title: Text('Welcome To Dr Shop',
               style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  fontWeight: FontWeight.normal,
               ),
-            ),
           ),
-          Spacer(),
-        Badge(
-          backgroundColor: Colors.red,
-          padding: EdgeInsets.all(7),
-          isLabelVisible: true,
-          label: Text(
-          "3",
-            style: TextStyle(color: Colors.black),
-          ),
-          child: InkWell(
-            onTap: (){Get.to(OrderFragmentScreen());
-              },
-            child:  Icon(
-              Icons.shopping_bag_outlined,
-              size:32,
-              color: Color(0xFF4C53A5),
-            ),
-          ),
+          backgroundColor: Colors.grey,
         ),
-        ],
       ),
     );
   }
