4126292494bd52471a5f01030702c5df3018be49
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-06-21 20:17:55 +0330

    fix : appbar color and widgets

--- lib/widgets/homeAppBar.dart
+++ lib/widgets/homeAppBar.dart
@@ -5,17 +5,17 @@ class HomeAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(height:75,
-      child: Container(
-        child: AppBar(
-          title: const Text('Welcome To Dr Shop',
-              style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-              ),
+    return SizedBox(
+      height: 75,
+      child: AppBar(
+        title: const Text(
+          'Welcome To Dr Shop',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.normal,
           ),
-          backgroundColor: Colors.grey,
         ),
+        backgroundColor: Theme.of(context).cardColor,
       ),
     );
   }
